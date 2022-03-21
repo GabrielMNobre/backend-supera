75aa624ead97a438596fb1b6d36f6df6d5475de9
GabrielMNobre <gmn.nobre@gmail.com>
2022-03-21 19:16:37 -0300

    Adicionando API completa

---
+++ BackendSupera/Controllers/ValidaCpfController.cs
@@ -0,0 +1,26 @@
+﻿using BackendSupera.Model;
+using BackendSupera.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendSupera.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValidaCpfController : ControllerBase
+    {
+        [HttpPost(Name = "PostCpf")]
+        public String PostCpf(Cpf request)
+        {
+            Cpf cpf = ValidaCpfService.ValidaCpf(request);
+
+            if (cpf.Valido)
+            {
+                return "True";
+            } else
+            {
+                return "False";
+            }
+
+        }
+    }
+}
---
+++ BackendSupera/Model/Cpf.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendSupera.Model
+{
+    public class Cpf
+    {
+
+        public string NumeroCpf { get; set; }
+
+        public bool Valido { get; set; }
+    }
+}
